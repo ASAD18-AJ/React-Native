fde167dc866343f28538e84a2b3308ba97f5e7e4
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2026-05-31 23:20:02 +0530

    Commit

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Image, ScrollView, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 
 export default function SignIn() {
   return (
     <ScrollView>
       <View>
         <Image />
+        <Text>Welcome back</Text>
       </View>
     </ScrollView>
   )
