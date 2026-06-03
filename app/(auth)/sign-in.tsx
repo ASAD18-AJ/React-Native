c448034f1ed2f81e3497641dccac77bbaeb51341
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2026-06-03 23:59:41 +0530

    commit

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Image, ScrollView, Text, TextInput, View } from 'react-native'
 
 export default function SignIn() {
   return (
@@ -7,6 +7,13 @@ export default function SignIn() {
       <View>
         <Image />
         <Text>Welcome back</Text>
+        <Text>Sign in to your account</Text>
+        <TextInput
+          placeholder='Email address'
+          placeholderTextColor="#9CA3AF"
+          value={''}
+          onChange={}
+        />
       </View>
     </ScrollView>
   )
