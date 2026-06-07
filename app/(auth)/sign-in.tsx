0da0e2d3bec03e496d22bfa3ad1b85ccae8abdab
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2026-06-07 23:59:28 +0530

    commit

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Image, ScrollView, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react';
+import { Image, ScrollView, Text, TextInput, View } from 'react-native';
 
 export default function SignIn() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [code, setCode] = useState("");
   return (
-    <ScrollView>
-      <View>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      className="bg-white"
+      keyboardShouldPersistTaps="handled"  
+    >
+      <View className="flex-1 justify-center px-6">
         <Image />
         <Text>Welcome back</Text>
         <Text>Sign in to your account</Text>
         <TextInput
           placeholder='Email address'
           placeholderTextColor="#9CA3AF"
-          value={''}
-          onChange={}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType='email-address'
+          autoCapitalize='none'
         />
       </View>
     </ScrollView>
