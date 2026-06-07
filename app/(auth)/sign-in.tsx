2b7cf6222dd216bab8ecebd17c51a5317b561f7c
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2026-06-08 00:08:00 +0530

    commit

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Image, ScrollView, Text, TextInput, View } from 'react-native';
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function SignIn() {
 
@@ -12,8 +12,10 @@ export default function SignIn() {
       className="bg-white"
       keyboardShouldPersistTaps="handled"  
     >
-      <View className="flex-1 justify-center px-6">
-        <Image />
+      <View className="flex-1 justify-center px-6 py-12">
+        <Image source={require("../../assets/images/kribb.png")}
+        resizeMode='contain'       
+        />
         <Text>Welcome back</Text>
         <Text>Sign in to your account</Text>
         <TextInput
@@ -24,6 +26,19 @@ export default function SignIn() {
           keyboardType='email-address'
           autoCapitalize='none'
         />
+
+        <TextInput
+          className="w-full border border-gray-300 rounded-md px-4 py-3 mb-6"
+          placeholder="Password"
+          placeholderTextColor="#9CA3AF"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <TouchableOpacity className="w-full bg-blue-600 py-4 rounded-xl items-center mb-4">
+
+        </TouchableOpacity>
       </View>
     </ScrollView>
   )
